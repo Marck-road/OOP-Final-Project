296f8aae703b7f249af954f1fafd55d3aad8e553
Marck-road <109259291+Marck-road@users.noreply.github.com>
2023-05-07 11:50:29 +0800

    Initial FIles
    Added Initial Files into the system

---
+++ ImpossibleGame/src/application/Main.java
@@ -0,0 +1,48 @@
+	package application;
+		
+	import java.io.File;
+
+//import java.awt.Image;
+	
+	import javafx.application.Application;
+	import javafx.fxml.FXMLLoader;
+	import javafx.stage.Stage;
+	import javafx.scene.Parent;
+	import javafx.scene.Scene;
+	import javafx.scene.image.Image;
+	import javafx.scene.image.ImageView;
+	import javafx.scene.layout.BorderPane;
+	import javafx.scene.layout.AnchorPane;
+	
+	
+	public class Main extends Application {
+		@Override
+		public void start(Stage primaryStage) {
+			try {
+				
+			
+				
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+				Parent root = loader.load();
+				Scene scene = new Scene(root);
+	
+				Image image1 = new Image(new File("src/images/logo.png").toURI().toString());
+				MainSceneController controller = loader.getController();
+				ImageView view1 = controller.view1;
+	
+				view1.setImage(image1);
+	
+				primaryStage.setScene(scene);
+				primaryStage.show();
+	
+	
+	
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public static void main(String[] args) {
+			launch(args);
+		}
+	}
