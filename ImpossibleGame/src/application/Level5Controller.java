7bd8e8439656867acaa7e5ac4dd880b99ac7512a
Ervin_Joshua <17100409@usc.edu.ph>
2023-05-23 00:09:31 +0800

    Added Level5

---
+++ ImpossibleGame/src/application/Level5Controller.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+
+public class Level5Controller extends LevelController {
+	
+
+	@FXML ImageView you;
+	public void initialize(Scene scene, LevelTransition levelTransition) {
+		System.out.println("Initializing Level 5 Controller");
+        super.initialize(scene, levelTransition);
+        Rat rat = getRat();
+        
+        
+	}
+	
+	
+	
+	
+	 
+	 
+	 
+}
+
