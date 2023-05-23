397ab978aa0af41004ec09d2e3bf06f9c046c292
Marck-road <109259291+Marck-road@users.noreply.github.com>
2023-05-23 08:36:56 +0800

    Level 3 working

--- ImpossibleGame/src/application/Level3Controller.java
+++ ImpossibleGame/src/application/Level3Controller.java
@@ -14,10 +14,10 @@ import javafx.util.Duration;
 public class Level3Controller extends LevelController{
 	
 	@FXML ImageView you;
-	@FXML private Line l1;
-	@FXML private Rectangle dangerZone;
+	@FXML private Line dangerZone;
+	@FXML private Rectangle l1;
     private Timeline timeline = null; 
-    private double speed = 3.0;
+    private double speed = 15.0;
 
 	public void initialize(Scene scene, LevelTransition levelTransition) {	
 		System.out.println("Initializing Level 3 Controller");
@@ -38,8 +38,8 @@ public class Level3Controller extends LevelController{
 	private void moveLine() {
 		timeline = new Timeline(new KeyFrame(Duration.millis(16), event -> {
 
-			double startX = l1.getStartX();
-	        double endX = l1.getEndX();
+			double startX = dangerZone.getStartX();
+	        double endX = dangerZone.getEndX();
 
 			if (startX <= 0 && endX <= 0) {
 	            speed = Math.abs(speed); // Change direction to move right
@@ -60,18 +60,18 @@ public class Level3Controller extends LevelController{
 	}
 	
 	private void updatePosition(double deltaX, double deltaY) {
-	    double newEndX = l1.getEndX() + deltaX;
-	    double newEndY = l1.getEndY() + deltaY;
+	    double newEndX = dangerZone.getEndX() + deltaX;
+	    double newEndY = dangerZone.getEndY() + deltaY;
 
 	    // Calculate the new start position based on the difference between the new and old end positions
-	    double newStartX = l1.getStartX() + (newEndX - l1.getEndX());
-	    double newStartY = l1.getStartY() + (newEndY - l1.getEndY());
+	    double newStartX = dangerZone.getStartX() + (newEndX - dangerZone.getEndX());
+	    double newStartY = dangerZone.getStartY() + (newEndY - dangerZone.getEndY());
 
 	    // Update the positions of the line's start and end points
-	    l1.setStartX(newStartX);
-	    l1.setStartY(newStartY);
-	    l1.setEndX(newEndX);
-	    l1.setEndY(newEndY);
+	    dangerZone.setStartX(newStartX);
+	    dangerZone.setStartY(newStartY);
+	    dangerZone.setEndX(newEndX);
+	    dangerZone.setEndY(newEndY);
 	    
 	    
 	    
